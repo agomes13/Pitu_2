d33b5996b67b1ced861ceaa33866bdc0e8fda458
agomes13 <agomes_13@yahoo.com.br>
2020-12-10 13:14:39 -0300

    projeto completo funcionando antes do build

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -90,7 +90,7 @@ class HomePage extends React.Component{
                                                 <Button variant="outline-secondary" onClick={()=> this.copyToClipboard()}>Copiar</Button>
                                             </InputGroup.Append>
                                         </InputGroup>
-                                        <p>Para acompanhar aas estatísticas, acesse {vars.HOST_APP + code}</p>
+                                        <p>Para acompanhar aas estatísticas, acesse {vars.HOST_APP + code}/stats</p>
                                     </>
                                 )
                             )
